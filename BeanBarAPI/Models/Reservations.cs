1f3c8b3af8308bf932200994157f1a30e47c1f72
K1dnextd00r <tkekanajnr@gmail.com>
2025-05-21 14:12:32 +0200

    Added reservation.cs model

---
+++ BeanBarAPI/Models/Reservations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Reservations
+{
+    [Key]
+    public int TableNum { get; set; }
+
+    [Required]
+    public DateTime ReservationDate { get; set; }
+
+    [Required]
+    public TimeSpan ReservationTime { get; set; }
+
+    [Required]
+    public int PartySize { get; set; }
+
+    [Required]
+    [StringLength(13)]
+    public string IDNumber { get; set; }
+
+    public string? CustomerName { get; set; }
+}
