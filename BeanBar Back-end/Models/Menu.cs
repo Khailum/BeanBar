26c8e3e763bd0ecd637416bc2b646d97f87cb380
K1dnextd00r <tkekanajnr@gmail.com>
2025-05-22 08:38:33 +0200

    Fixed problems regarding previous files

---
+++ BeanBar Back-end/Models/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeanBar_Back_end.Models
+{
+    public class Menu
+    {
+        [Key]
+        public int ItemID { get; set; }
+
+        [Required, MaxLength(100)]
+        public string ItemName { get; set; }
+
+        [Required, MaxLength(50)]
+        public string ItemType { get; set; } // "Hot", "Cold", "Snack"
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
