b2dccd4a28cbfc839c144d3aeb3f26c5936a6cfb
K1dnextd00r <tkekanajnr@gmail.com>
2025-07-02 12:52:52 +0200

    Migrations

--- BeanBarBack-end/BeanBarAPI/Migrations/20250702105146_RemoveUserFromReservations.Designer.cs
+++ BeanBarBack-end/BeanBarAPI/Migrations/20250702105146_RemoveUserFromReservations.Designer.cs
@@ -4,6 +4,7 @@ using BeanBarAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BeanBar_Back_end.Migrations
 {
     [DbContext(typeof(CoffeeDBcontext))]
-    partial class CoffeeDBcontextModelSnapshot : ModelSnapshot
+    [Migration("20250702105146_RemoveUserFromReservations")]
+    partial class RemoveUserFromReservations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BeanBarBack-end/BeanBarAPI/Migrations/20250702105146_RemoveUserFromReservations.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeanBar_Back_end.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUserFromReservations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TableReservations_Users_CustomerID",
+                table: "TableReservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TableReservations_CustomerID",
+                table: "TableReservations");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerID",
+                table: "TableReservations");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "TableReservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryOption",
+                table: "Cart",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Cart",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "TableReservations");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveryOption",
+                table: "Cart");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Cart");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerID",
+                table: "TableReservations",
+                type: "nvarchar(13)",
+                maxLength: 13,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TableReservations_CustomerID",
+                table: "TableReservations",
+                column: "CustomerID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TableReservations_Users_CustomerID",
+                table: "TableReservations",
+                column: "CustomerID",
+                principalTable: "Users",
+                principalColumn: "Email",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
